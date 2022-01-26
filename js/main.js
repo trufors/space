2b1c0d3705e70f53fe4c120085c006b7a232896f
trufors <yungbatya@icloud.com>
2022-01-26 19:12:08 +0300

    fullinfo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrowleft" src="images/arrowleft.svg" alt="">',
+        nextArrow: '<img class="slider__arrow slider__arrowright" src="images/arrowright.svg" alt="">',
+    });
 });
